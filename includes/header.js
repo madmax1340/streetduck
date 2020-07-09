fd43bce9f83788472bc1fbfd37404ca74b419ce7
otherMax <45923941+otherMax@users.noreply.github.com>
2020-07-09 17:48:21 -0500

    Add files via upload

--- includes/header.js
+++ includes/header.js
@@ -1,1 +1,16 @@
-document.write('<h1>cringe</h1>');
+//navbar
+document.write('<nav class="navbar navbar-light navbar-expand-md bg-white justify-content-between"><div class="container"><button class="navbar-toggler" type="button" data-toggle="collapse"><span class="navbar-toggler-icon"></span></button><div class="collapse navbar-collapse w-50" ><div class="navbar-nav"><a class="nav-item nav-link" href="../../browse.html">Browse</a><a class="nav-item nav-link" href="../../members.html">Members</a></div></div><a href="/" class="navbar-brand mx-auto text-center title"><strong>Street Duck</strong></a><div class="navbar-collapse collapse w-50"><div class="navbar-nav ml-auto"><a class="nav-item nav-link" href="../../contact.html">Contact</a></div></div></div></nav>');
+
+/*<nav class="navbar sticky-top navbar-expand-lg navbar-light bg-white">
+<a class="navbar-brand" href="#">Street Duck</a>
+<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+<div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+  <div class="navbar-nav">
+    <a class="nav-item nav-link" href="../../">Home</a><a class="nav-item nav-link" href="../../browse.html">Browse</a>
+    <a class="nav-item nav-link" href="../../members.html">Members</a><a class="nav-item nav-link" href="../../contact.html">Contact</a>
+  </div>
+  <div class="navbar-nav ml-auto"><a class="nav-item nav-link" href="../../login.html">Login</a>
+  </div>
+</div>
+</nav>*/
+
---
+++ includes/links.js
@@ -0,0 +1,1 @@
+document.write('<meta charset="utf-8"><meta name="viewport" content="width=device-width, initial-scale=1"><link rel="stylesheet" href="styles.css"><link href="https://fonts.googleapis.com/css2?family=Balsamiq+Sans:wght@400;700&display=swap" rel="stylesheet"><link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">');
