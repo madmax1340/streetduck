f666a191e5b4bbf1e28aaa3e21a1a13276e6c751
otherMax <45923941+otherMax@users.noreply.github.com>
2020-07-10 11:09:36 -0500

    Delete header.js

--- header.js
+++
@@ -1,2 +0,0 @@
-//navbar
-document.write('<nav class="navbar navbar-light navbar-expand-md bg-white justify-content-between"><div class="collapse navbar-collapse w-50" ><div class="navbar-nav"><a class="nav-item nav-link" href="../../browse.html">Browse</a><a class="nav-item nav-link" href="../../about.html">About</a></div></div><a href="/" class="navbar-brand mx-auto text-center title"><strong>Street Duck</strong></a><div class="navbar-collapse collapse w-50"><div class="navbar-nav ml-auto"><a class="nav-item nav-link" href="../../contact.html">Contact</a></div></div></nav>');
