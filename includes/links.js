6118cd80bf7fec2bac2e252b108a20d57ee58b82
otherMax <45923941+otherMax@users.noreply.github.com>
2020-07-10 15:01:01 -0500

    Update links.js

--- includes/links.js
+++ includes/links.js
@@ -1,1 +1,4 @@
+/* Google Analytics*/
+document.write('<script async src="https://www.googletagmanager.com/gtag/js?id=UA-131762973-3"></script><script>window.dataLayer = window.dataLayer || [];function gtag(){dataLayer.push(arguments);}gtag("js", new Date());gtag("config", "UA-131762973-3");</script>');
+/*Other Links for HEAD*/
 document.write('<meta charset="utf-8"><meta name="viewport" content="width=device-width, initial-scale=1"><link rel="stylesheet" href="styles.css"><link href="https://fonts.googleapis.com/css2?family=Balsamiq+Sans:wght@400;700&display=swap" rel="stylesheet"><link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">');
